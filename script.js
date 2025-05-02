0c8fd7ec97b28d5100dbd10bd59f45640c8d3463
antoniorafaelsouzaneves <202415210056@ifba.edu.br>
2025-05-02 14:33:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener('scroll', function() {
+    const button = document.getElementById('back-to-top');
+    if (window.scrollY > 300) {
+        button.style.display = 'block';
+    } else {
+        button.style.display = 'none';
+    }
+});
+document.getElementById('back-to-top').addEventListener('click', function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
